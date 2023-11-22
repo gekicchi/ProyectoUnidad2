0a93a5cb40b81e68acfa3ae340bb97731096dcd6
gekicchi <oyarzunm18@gmail.com>
2023-11-22 00:14:07 -0300

    clase mapa funcional

--- TheGuardiansBattles.cpp
+++ TheGuardiansBattles.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <sstream>
 #include <unordered_map>
+#include <stack>
 using namespace std;
 
 struct Guardian
@@ -76,38 +77,53 @@ class Ranking
 class Map
 {
 	public:
-		Map() {}
+		Map(vector<vector<int>> p) { paths = p;}
 		~Map() {}
+		
+		void setCityNames(int num, string name) { cityNames[num] = name; }
+		unordered_map<int, string> getCityNames() { return cityNames; }
 		
 		void AddPath(int start, int end);
 		bool AreAdjacent(int start, int end);
 		bool FindPath(int start, int end);
+		void Print();
 		
 	private:
-		vector<vector<pair<int>>> paths;
+		vector<vector<int>> paths;
 		unordered_map<int, string> cityNames;
 };
 
-void ReadGuardiansText(char *file_name, Ranking *ranking, Hierarchy *hierarchy);
+void ReadGuardiansFile(char *file_name, Ranking *ranking, Hierarchy *hierarchy);
+void ReadCitiesFile(char *file_name, Map *map);
+void StartEndPath(Map map, int *start, int *end);
 
 int main(int argc, char *argv[])
 {
-	int election;
+	int election, subElection, start, end;
 	
 	Ranking ranking;
 	Hierarchy hierarchy;
+	vector<vector<int>> list(20);
+	Map map(list);
 	
-	ReadGuardiansText(argv[1], &ranking, &hierarchy);
+	ReadGuardiansFile(argv[1], &ranking, &hierarchy);
+	ReadCitiesFile(argv[2], &map);
 	
 	ranking.Print();
 	cout << endl << endl << endl;
 	hierarchy.Print();
+	cout << endl << endl << endl;
+	map.Print();
+	
+	map.FindPath(0,2);
+	map.FindPath(3,10);
 	
 	do{
 		cout << "----The Guardian's Battles----" << endl;
 		cout << "[1] Ver Lista de Candidatos" << endl << "[2] Ver Guardian" << endl << "[3] Conocer el Reino" << endl << "[4] Presenciar una Batalla" << endl << "[5] Salir" << endl;
 		cout << "Eleccion: ";
 		cin >> election;
+		cout << endl;
 		
 		switch(election)	
 		{
@@ -122,6 +138,21 @@ int main(int argc, char *argv[])
 				
 			case 3: // conocer reino
 				cout << "CONOCIENDO REINO" << endl;
+				cout << "[1] Construir Camino\n[2] Consultar Camino";
+				cout << "Eleccion: ";
+				cin >> subElection;
+				switch (subElection)
+				{
+					case 1: // construir camino
+						StartEndPath(map, &start, &end);
+						map.AddPath(start, end);
+						break;
+						
+					case 2: // consultar camino
+						StartEndPath(map, &start, &end);
+						map.FindPath(start, end);
+						break;
+				}
 				break;
 				
 			case 4: // presenciar batalla
@@ -186,17 +217,25 @@ void Ranking::PrintRanking(RankedGuardian *current)
 		return;
 		
 	PrintRanking(current->right);
-	//cout << "Nombre: " << current->Info->Name << " | Ciudad: " << current->Info->City << " | Nivel: " << current->Info->Level << endl;
-	cout << current->Info->Level << ") " << current->Info->Name << endl;
+	
+	cout << current->Info->Level << ") " << current->Info->Name;
+	if (current->Info->Level >= 90 && current->Info->Level < 100)
+		cout << " (Candidato a Guardian)";
+	cout << endl;
+	
 	PrintRanking(current->left);
 }
 
 /**** FUNCIONES PARA EL MAPA ****/
-void AddPath(int start, int end)
+void Map::AddPath(int start, int end)
 {
+	if (start == end)
+		return;
+		
 	paths[start].push_back(end);
 }
-bool AreAdjacent(int start, int end)
+
+bool Map::AreAdjacent(int start, int end)
 {
 	for (int adj : paths[start])
 		if (adj == end)
@@ -204,16 +243,67 @@ bool AreAdjacent(int start, int end)
 			
 	return false;
 }
-bool FindPath(int start, int end)
+
+bool Map::FindPath(int start, int end)
 {
+	vector<bool> visited(cityNames.size(), false);
+	vector<int> parent(cityNames.size(),-1);
+	vector<int> path;
+	stack<int> stack;
 	
+	stack.push(start);
+	visited[start] = true;
 	
+	while(!stack.empty())
+	{
+		int curr = stack.top();
+		stack.pop();
+				
+		if (curr == end)
+		{
+			for (int i=end; i != -1; i = parent[i])
+				path.push_back(i);
+						
+			for (int i=path.size()-1; i>-1; i--)
+			{
+				cout << cityNames[path[i]];
+				if (i>0)
+					cout << " -> ";
+			}
+			
+			cout << endl;
+			return true;
+		}
+				
+		for (int adj : paths[curr])
+		{
+			if (!visited[adj])
+			{
+				stack.push(adj);
+				visited[adj] = true;
+				parent[adj] = curr;
+			}
+		}
+	}
+	
+	cout << "No Existe Camino entre " << cityNames[start] << " y " << cityNames[end] << endl;
 	return false;
+}
+
+void Map::Print()
+{
+	for (int i=0; i<cityNames.size(); i++)
+	{
+		cout << "[" << i << "] " << cityNames[i] << ": ";
+		for (int adj : paths[i])
+			cout << cityNames[adj] << "  ";
+		cout << endl;
+	}
 }
 
 /**** RESTO DE FUNCIONES ****/
 // lee archivo de guardianes
-void ReadGuardiansText(char *file_name, Ranking *ranking, Hierarchy *hierarchy)
+void ReadGuardiansFile(char *file_name, Ranking *ranking, Hierarchy *hierarchy)
 {
 	ifstream guardians(file_name);
 	
@@ -258,7 +348,69 @@ void ReadCitiesFile(char *file_name, Map *map)
 	
 	if (cities.is_open())
 	{
-		
+		string line;
+		while (getline(cities, line))
+		{
+			istringstream ss(line);
+			string chunk;
+			vector<string> chunks;
+			int cityPos, connectionPos;
+			bool cityExists = false, connectionExists = false;
+			
+			while (getline(ss,chunk,','))
+				chunks.push_back(chunk);
+			
+			string connection = chunks.back();
+			chunks.pop_back();
+			
+			string city = chunks.back();
+			chunks.pop_back();
+			
+			for (int i=0; i<map->getCityNames().size(); i++)
+			{
+				if (cityExists && connectionExists)
+					break;
+				
+				if (map->getCityNames()[i] == city )
+				{
+					cityPos = i;
+					cityExists = true;
+				}
+					
+				if (map->getCityNames()[i] == connection)
+				{
+					connectionPos = i;
+					connectionExists = true;
+				}
+			}
+			
+			if (!cityExists)
+			{
+				cityPos = map->getCityNames().size();
+				map->setCityNames(map->getCityNames().size(), city);
+			}
+			
+			if (!connectionExists)
+			{
+				connectionPos = map->getCityNames().size();
+				map->setCityNames(map->getCityNames().size(), connection);
+			}
+			
+			map->AddPath(cityPos, connectionPos);
+		}
 	}
 	cities.close();
+}
+
+void StartEndPath(Map map, int *start, int *end)
+{
+	cout << "Inicio de Camino" << endl;
+	map.Print();
+	cout << endl<< "Eleccion: ";
+	cin >> *start;
+	
+	cout << "Final de Camino" << endl;
+	map.Print();
+	cout << endl << "Eleccion: ";
+	cin >> *end;
 }
